cf4029ff703e44f95a221dde826c54dd708065e9
eduardanfreitas <dudanfreitas@hotmail.com>
2016-05-24 14:52:40 -0300

    Add files via upload

---
+++ Ex1.cpp
@@ -0,0 +1,16 @@
+//1) Fazer um algoritmo que leia um número e imprima o valor lido caso seja maior que 50.
+
+#include<stdio.h>
+#include<conio.h>
+ 
+ main()
+ {
+ int numero;
+ 
+ printf("Informe um número: \n");
+ scanf("%d",&numero);
+ if (numero>50);
+ {
+ printf("O numero é : %d",numero);
+}
+}
